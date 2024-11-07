69f0280f38fef9ad8e1ee3ec1da466255263ea38
charleez <extrabutterychar@gmail.com>
2024-11-06 18:48:06 -0800

    Made ball and chain now its becoming one with each other needs fix

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 mousePosition;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] public float force = 10f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition.z = 0;
+        Debug.Log(mousePosition.x);
+        Debug.Log(mousePosition.y);
+        rb.AddForce((mousePosition - transform.position).normalized * force);
+
+        
+
+    }
+}
