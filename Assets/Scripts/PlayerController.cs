3e6e1c78526dbbc8d64376a2c017b9ddc9c5a27c
Brian Feng <brainfeng135@gmail.com>
2024-11-06 17:28:22 -0800

    help me please

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class CharacterController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public Vector3 velocity;
+    public float MAX_SPEED;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(velocity * Time.deltaTime*MAX_SPEED, Space.Self);
+    }
+
+    public void Move(InputAction.CallbackContext context){
+        Vector2 move = context.ReadValue<Vector2>();
+        velocity.x = move.x;
+        velocity.z = move.y;
     }
 }
