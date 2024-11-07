c21ee98d384c1d369a46c060ab9a47798917342d
Brian Feng <brainfeng135@gmail.com>
2024-11-06 18:00:59 -0800

    left right movement good

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,28 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public Vector3 velocity;
-    public float MAX_SPEED;
+    public float SPEED;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(velocity * Time.deltaTime*MAX_SPEED, Space.Self);
+        velocity = Vector3.zero;
+        if(Input.GetAxis("Horizontal") != 0){
+            velocity.x = Input.GetAxis("Horizontal");
+        }
+        velocity.Normalize();
+        transform.Translate(velocity*Time.deltaTime*SPEED);
+        Debug.Log(velocity);
+        Debug.Log(Input.GetAxis("Horizontal"));
     }
 
-    public void Move(InputAction.CallbackContext context){
-        Vector2 move = context.ReadValue<Vector2>();
-        velocity.x = move.x;
-        velocity.z = move.y;
-    }
+    
 }
